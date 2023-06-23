9b225e610f9883e8de9b22bf6861b8c032f04d60
Ilyasse Fouaide <ilyasse.fouaide@gmail.com>
2023-06-23 20:21:48 +0100

    add all the needed tables

---
+++ V2_VIEWS/app/Http/Controllers/EfmController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Efm;
+use Illuminate\Http\Request;
+
+class EfmController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Efm $efm)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Efm $efm)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Efm $efm)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Efm $efm)
+    {
+        //
+    }
+}
